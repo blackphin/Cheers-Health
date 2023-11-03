08bf237fc70aaff66f09d9f6011c5dfd7e39fe33
Shivansh Goel <shivanshgoel009@gmail.com>
2023-11-04 00:34:28 +0530

    Added auth layer

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,51 +1,53 @@
+from datetime import datetime, timedelta
+
 from jose import JWTError, jwt
-import schemas
+
+
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from config import settings
+from sqlalchemy.orm import Session
 
-from datetime import datetime, timedelta
+import schemas_request
+import database
+import models
+from config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-SECRET_KEY = settings.secret_key
-ALGORITHM = settings.algorithm
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-
-    expire = datetime.utcnow() + timedelta(minutes=99999999)
+    expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
     to_encode.update({"exp": expire})
-
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
+    encoded_jwt = jwt.encode(
+        to_encode, settings.secret_key, algorithm=settings.algorithm)
     return encoded_jwt
 
-def verify_access_token(token: str, credentials_exception):
 
+def verify_access_token(token: str):
     try:
-
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get("user_id")
-        if id is None:
-            raise credentials_exception
-        token_data = schemas.TokenData(id=id)
-
-    except JWTError:
-        raise credentials_exception
+        payload = jwt.decode(token, settings.secret_key,
+                             algorithms=[settings.algorithm])
+        if payload.get("reg_no") is not None:
+            reg_no: str = payload.get("reg_no")
+            if reg_no is None:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials", headers={"WWW-Authenticate": "Bearer"}
+                )
+            token_data = schemas_request.TokenData(reg_no=reg_no)
+    except JWTError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Couldn't Verify JWT", headers={"WWW-Authenticate": "Bearer"}
+        ) from exc
     return token_data
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-
-    token = verify_access_token(token, credentials_exception)
-
-    return token.id
-
-# print(create_access_token({"user_id":"497214b0-8422-4354-93bc-e0304837fc0c", "jti":"hello"}))
-# print(get_current_user("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDk3MjE0YjAtODQyMi00MzU0LTkzYmMtZTAzMDQ4MzdmYzBjIiwianRpIjoiaGVsbG8iLCJleHAiOjc2OTM4MTI5NzJ9.v6KbTD7Rd5Qd0cAy_-pJ5Wj8LW2qnfGCKuGa6RravKg"))
+    token_data = verify_access_token(token)
+    # user = db.query(models.MasterStudentsPersonal).filter(
+    #     models.MasterStudentsPersonal.reg_no == token_data.reg_no).first()
+    if token_data.reg_no is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials", headers={"WWW-Authenticate": "Bearer"}
+        )
+    return token_data.reg_no
