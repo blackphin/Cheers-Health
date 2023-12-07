6941092fcb02c4bebfcae954316837fff92da0fb
Shivansh Goel <shivanshgoel009@gmail.com>
2023-12-07 18:01:32 +0530

    removed create account functionality

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -36,24 +36,24 @@ def login(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.post('/createaccount', response_model=schemas.Token)
-def create_account(user_details: schemas.CreateUser, db: Session = Depends(database.get_db)):
-    existing_user = db.query(models.Users.id).filter(
-        models.Users.email == user_details.email).first()
-    if not existing_user:
-        user_details.password = utils.hash_pass(user_details.password)
-        new_user = models.Users(**user_details.dict())
-        db.add(new_user)
-        db.commit()
-        db.refresh(new_user)
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail="User already Exists"
-        )
-    user_id = db.query(models.Users.id).filter(
-        models.Users.email == user_details.email).first()
+# @router.post('/createaccount', response_model=schemas.Token)
+# def create_account(user_details: schemas.CreateUser, db: Session = Depends(database.get_db)):
+#     existing_user = db.query(models.Users.id).filter(
+#         models.Users.email == user_details.email).first()
+#     if not existing_user:
+#         user_details.password = utils.hash_pass(user_details.password)
+#         new_user = models.Users(**user_details.dict())
+#         db.add(new_user)
+#         db.commit()
+#         db.refresh(new_user)
+#     else:
+#         raise HTTPException(
+#             status_code=status.HTTP_409_CONFLICT, detail="User already Exists"
+#         )
+#     user_id = db.query(models.Users.id).filter(
+#         models.Users.email == user_details.email).first()
 
-    access_token = oauth2.create_access_token(
-        data={"user_id": str(user_id.id)})
+#     access_token = oauth2.create_access_token(
+#         data={"user_id": str(user_id.id)})
 
-    return {"access_token": access_token, "token_type": "bearer"}
+#     return {"access_token": access_token, "token_type": "bearer"}
